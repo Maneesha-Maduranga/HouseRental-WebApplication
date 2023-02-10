c2b6a0afcae3b039f9a7a3fb3fde9c64b46f53e7
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2023-02-10 11:51:01 +0530

    Listing modeld Done

---
+++ backend/models/Listing.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+const Joi = require('joi')
+
+
+const ListingSchema = new mongoose.Schema({
+      title: {
+        type: String,
+        required: [true],
+      },
+      address: {
+        type: String,
+        required: [true],
+      },
+      city: {
+        type: String,
+        required: [true],
+      },
+      description: {
+        type: String,
+      },
+      price:{
+        type:Number,
+        required: [true],
+      },
+      rooms:{
+        type:Number,
+        required: [true],
+      },
+      bedroom:{
+        type:Number,
+    }
+    
+});
+
+const Listing = mongoose.model('Listing', ListingSchema);
+
+
+const validate = (title,address,city,description,price,rooms,bedroom) => {
+
+
+    const schema = Joi.object({
+        title: Joi.string().required(),
+        address: Joi.string().required(),
+        city:Joi.string(),
+        description:Joi.string().max(150),
+        price:Joi.number().required(),
+        rooms:Joi.number().required(),
+        bedroom:Joi.number()
+
+    });
+    
+    return  schema.validate({ title,address,city,description,price,rooms,bedroom });
+
+}
